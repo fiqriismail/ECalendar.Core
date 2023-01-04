a561e927d543c8d9ab583768f19f999e344b8fe6
fiqriismail <fiqri.ismail@gmail.com>
2023-01-04 21:06:31 +0530

    Unit Tests Project for WEB API created

---
+++ ECalendar.Wb.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using FluentAssertions;
+
+namespace ECalendar.Wb.Api.Tests.Unit;
+
+public class DeleteMe
+{
+    [Fact]
+    public void ShouldBeAlwaysTrue()
+    {
+        Assert.True(true);
+    }
+}
